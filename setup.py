ed839ae327dd3d2b8cbee3e786df8da6aab957f0
emehmkh <mehmood.khan@ericsson.com>
2016-08-17 15:52:59 +0200

    adding setup and run files

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+setup(
+    name='BayesInfer',
+    version='0.1dev',
+    author='Mohamed Abdel Aziz, Mehmood Alam Khan',
+    author_email='mohamed.abdelaziz@ericsson.com, malagori@kth.se',
+    url='https://github.com/malagori/BayesCNN',
+    packages=['bayesCNN',],
+    scripts = ['runBayesCNN.py',],
+    license='GPLv3',
+    long_description=open('README.md').read(),
+    install_requires= ['numpy>=1.11.1'],
+)
